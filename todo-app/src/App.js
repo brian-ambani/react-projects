5a954db5d123699afa4c71fd226c97fc7ff3021f
brian-ambani <brainambani1@gmail.com>
2023-10-19 13:30:05 +0300

    updated App JSX

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,15 @@
 import styles from "./App.module.css";
+import { useState } from "react";
 import AddTodo from "./components/addTodo/AddTodo";
 import DisplayTodos from "./components/displayTodo/DisplayTodos";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className={styles.App}>
-      <AddTodo />
-      <DisplayTodos />
+      <AddTodo setInputText={setInputText} />
+      <DisplayTodos inputText={inputText} />
     </div>
   );
 }
