00ce1b869e7c6895b7e2fab62a0f0ac8d89ea4b8
brian-ambani <brainambani1@gmail.com>
2023-11-01 00:16:45 +0300

    updated the nav and hero

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,8 +1,10 @@
+import Hero from "./components/hero/Hero";
 import Nav from "./components/nav/Nav";
 function App() {
   return (
     <div className="App container">
       <Nav />
+      <Hero />
     </div>
   );
 }
---
+++ react-website/src/components/hero/Hero.jsx
@@ -0,0 +1,36 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <p className="p1">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
+        commodi eaque, dignissimos reprehenderit unde distinctio qui alias nam
+        repudiandae iure architecto vel impedit odio ipsum dolorum provident hic
+        animi soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Omnis reprehenderit sunt, mollitia est sapiente iure provident quos
+        commodi. Veritatis quos nam reiciendis a corporis cupiditate ab
+        repellendus porro velit quas ducimus placeat aspernatur quibusdam
+        quisquam, est atque perspiciatis, quidem harum, rerum sequi facilis.
+        Repellat, nesciunt soluta. Nesciunt modi iure voluptatem eveniet fugiat
+        natus labore consequuntur praesentium maiores ipsam dolores molestias
+        tempora beatae doloremque fuga, velit sint unde ut repudiandae molestiae
+        enim sit soluta reprehenderit. Quos tempore ipsum reiciendis deserunt
+        odio.
+      </p>
+      <p className="p2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatum
+        deserunt veritatis sint eum magnam optio? Illum ad sit, consequuntur a
+        iure repellendus eaque tempora doloribus ullam nihil placeat quaerat!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, rerum
+        natus? Voluptate similique obcaecati blanditiis assumenda commodi
+        ratione sit sequi, vitae iusto consequatur vel repellendus ducimus magni
+        dicta cum aperiam, eos fugit distinctio iste quo, alias id? Itaque,
+        reprehenderit nihil. Dolorum harum, necessitatibus modi magni debitis
+        eaque vitae fugit cupiditate in dolore impedit voluptas officiis sed
+        obcaecati expedita eum asperiores, ad ratione commodi. Odit ea nesciunt
+        dolore iste similique velit!
+      </p>
+    </div>
+  );
+}
