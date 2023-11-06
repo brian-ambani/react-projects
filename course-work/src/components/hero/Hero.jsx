72ad2ba3fd166760bd8105ec01fe15892b09e22d
brian-ambani <brainambani1@gmail.com>
2023-11-06 12:51:08 +0300

    updated the hero section

--- course-work/src/components/hero/Hero.jsx
+++ course-work/src/components/hero/Hero.jsx
@@ -5,7 +5,18 @@ export default function Hero() {
   return (
     <div className="hero">
       <div className="container">
-        <h2>Hero</h2>
+        <div className="hero_content">
+          <h4>Flin Tech</h4>
+          <h1>Web Development Projects For Every Business</h1>
+          <p>
+            Leading web development company dedicated to turning your digital
+            dreams into reality.
+          </p>
+          <button>
+            <a href="#">Get Started</a>
+          </button>
+        </div>
+        <div className="hero_stats"></div>
       </div>
     </div>
   );
