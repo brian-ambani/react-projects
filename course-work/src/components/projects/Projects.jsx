2a59945a2b10a52a9b62086027ca2ad83e0fa7a6
brian-ambani <brainambani1@gmail.com>
2023-11-06 22:20:19 +0300

    updated the projects section

--- course-work/src/components/projects/Projects.jsx
+++ course-work/src/components/projects/Projects.jsx
@@ -2,30 +2,80 @@ import React from "react";
 import "./Projects.css";
 export default function Projects() {
   return (
-    <div className="projects_section">
+    <div className="project_section">
       <div className="container">
         <div className="latest-projects">
-          <h1>
+          <h1 className="card-title">
             Latest <span>Projects</span>
           </h1>
-          <div className="card">
-            <h3>INTERMIDIATE</h3>
-            <h1>Web Development</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatibus, quod, quia, doloremque voluptatum quae doloribus
-              voluptatem repellat quibusdam quos nemo. Quisquam voluptatibus,
-              quod, quia, doloremque voluptatum quae doloribus voluptatem
-              repellat quibusdam quos nemo.
-            </p>
+          <div className="cards">
+            <div className="card">
+              <div className="card-content">
+                <h3>
+                  <span>INTERMIDIATE</span>
+                </h3>
+                <h1>Web Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-content">
+                <h3>
+                  <span>INTERMIDIATE</span>
+                </h3>
+                <h1>Web Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-content">
+                <h3>
+                  <span>INTERMIDIATE</span>
+                </h3>
+                <h1>Web Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="popular-projects">
-          <h1>
+          <h1 className="card-title">
             Popular <span>Projects</span>
           </h1>
+          <div className="cards">
+            <div className="card">
+              <div className="card-content">
+                <h3>
+                  <span>INTERMIDIATE</span>
+                </h3>
+                <h1>Web Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-content">
+                <h3>
+                  <span>INTERMIDIATE</span>
+                </h3>
+                <h1>Web Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-content">
+                <h3>
+                  <span>INTERMIDIATE</span>
+                </h3>
+                <h1>Web Development</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <a className="btn" href="">
+        Show More
+      </a>
     </div>
   );
 }
