ea357c87004f033fc0de7a8dc96de3bd9ac34809
brian-ambani <brainambani1@gmail.com>
2023-11-10 02:52:56 +0300

    updated  tailwindcss

--- website-2/src/App.js
+++ website-2/src/App.js
@@ -4,6 +4,38 @@ import { useState } from "react";
 
 function App() {
   const [role, setRole] = useState("Prodct Engineer");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Brian",
+      role: "Founder",
+      img: "https://images.pexels.com/photos/18938081/pexels-photo-18938081/free-photo-of-portrait-of-an-elderly-man-wearing-a-cap.jpeg",
+    },
+    {
+      name: "Mary",
+      role: "Co-Founder",
+      img: "https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg",
+    },
+    {
+      name: "Kate",
+      role: "Project Manager",
+      img: "https://images.pexels.com/photos/3797438/pexels-photo-3797438.jpeg",
+    },
+    {
+      name: "Jane",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/2625122/pexels-photo-2625122.jpeg",
+    },
+    {
+      name: "Rakesh",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2232981/pexels-photo-2232981.jpeg",
+    },
+    {
+      name: "Micah",
+      role: "Web Designer",
+      img: "https://images.pexels.com/photos/1559487/pexels-photo-1559487.jpeg",
+    },
+  ]);
 
   const ShowEmpoloyees = true;
   return (
@@ -18,49 +50,17 @@ function App() {
             }}
           />
 
-          <div className="flex flex-wrap">
-            <Employee
-              name="Brian"
-              role="Intern"
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Mike"
-              role={role}
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Micah"
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Brian"
-              role="Intern"
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Mike"
-              role={role}
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Micah"
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Brian"
-              role="Intern"
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Mike"
-              role={role}
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
-            <Employee
-              name="Micah"
-              img="https://avatars.githubusercontent.com/u/1000223?v=4"
-            />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              console.log(employee);
+              return (
+                <Employee
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
