681e6b4766c1ce6c925d59b45fcd19bfef527a0f
brian-ambani <brainambani1@gmail.com>
2023-11-11 02:32:19 +0300

    added completed the modal

--- website-2/src/components/Employee.jsx
+++ website-2/src/components/Employee.jsx
@@ -13,10 +13,6 @@ function Employee({ name, role, img }) {
           <p className="text-slate-500 font-medium">{role}</p>
         </div>
         <EditEmployee />
-
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Update
-        </button>
       </div>
     </div>
   );
