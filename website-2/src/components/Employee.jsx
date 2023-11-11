cde799868ce2b7bb1254d840037c2e381469726f
brian-ambani <brainambani1@gmail.com>
2023-11-11 12:58:19 +0300

    updated the form in the modal

--- website-2/src/components/Employee.jsx
+++ website-2/src/components/Employee.jsx
@@ -2,7 +2,7 @@ import EditEmployee from "./EditEmployee";
 
 function Employee({ name, role, img }) {
   return (
-    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div className="min-w-[350px] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <img
         className="object-cover rounded-full h-[100px] w-[100px]  block mx-auto  sm:mx-0 sm:shrink-0"
         src={img}
@@ -12,7 +12,7 @@ function Employee({ name, role, img }) {
           <p className="text-lg text-black font-semibold">{name}</p>
           <p className="text-slate-500 font-medium">{role}</p>
         </div>
-        <EditEmployee />
+        <EditEmployee name={name} role={role} />
       </div>
     </div>
   );
