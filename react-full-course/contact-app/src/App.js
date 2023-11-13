7c7f29e99d78a0b690d0debc8d175a411100dcda
brian-ambani <brainambani1@gmail.com>
2023-11-13 22:14:56 +0300

    updated the App.js file

--- react-full-course/contact-app/src/App.js
+++ react-full-course/contact-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Forms from "./components/Forms";
 import Lists from "./components/Lists";
+import AppUser from "./components/props/AppUser";
 
 function App() {
   return (
     <div>
       <h1>REACT COMPLETE COURSE</h1>
-      <Lists />
+      {/* <Lists /> */}
+      <AppUser />
       {/* <Forms /> */}
     </div>
   );
