1d31a8c5d7c8eaaf8c397da3ef95da14a9cc3000
brian-ambani <brainambani1@gmail.com>
2023-11-13 21:44:55 +0300

    updated Lists of objects in react

--- react-full-course/contact-app/src/components/Lists.jsx
+++ react-full-course/contact-app/src/components/Lists.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 
 export default function Lists() {
-  const names = ["John", "Paul", "George", "Ringo"];
+  //simple list
+  //   const names = ["John", "Paul", "George", "Ringo"];
+  //   return (
+  //     <div>
+  //       {names.map((name) => {
+  //         return <h2 key={Math.random() * 10}>{name}</h2>;
+  //       })}
+  //     </div>
+  //   );
+
+  // Arrays of objects
+  const Users = [
+    { name: "Brian", age: 25, role: "Admin" },
+    { name: "John", age: 20, role: "User" },
+    { name: "Mary", age: 18, role: "User" },
+  ];
+
   return (
-    <div>
-      {names.map((name) => {
-        return <h2 key={Math.random() * 10}>{name}</h2>;
-      })}
-    </div>
+    <>
+      {Users.map((user) => (
+        <ul key={Math.random() * 10}>
+          <li>{user.name}</li>
+          <li>{user.age}</li>
+          <li>{user.role}</li>
+        </ul>
+      ))}
+    </>
   );
 }
