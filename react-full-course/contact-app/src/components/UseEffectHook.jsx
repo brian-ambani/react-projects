c71b01fdfdef0a23a53b16a1514d23dc6007082a
brian-ambani <brainambani1@gmail.com>
2023-11-15 03:07:42 +0300

    updated the useEffect

---
+++ react-full-course/contact-app/src/components/UseEffectHook.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export default function UseEffectHook() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const data = await response.json();
+      setData(data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((x) => (
+        <h2 key={Math.random()}>{x.title}</h2>
+      ))}
+    </div>
+  );
+}
