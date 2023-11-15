77ddc41dea1d731de7a37ea0e5da7d73037dbc64
brian-ambani <brainambani1@gmail.com>
2023-11-15 03:08:22 +0300

    updated the App.js file

--- react-full-course/contact-app/src/App.js
+++ react-full-course/contact-app/src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 import Forms from "./components/Forms";
 import Lists from "./components/Lists";
 import AppUser from "./components/props/AppUser";
+import UseEffectHook from "./components/UseEffectHook";
 
 function App() {
   return (
     <div className="app">
       <h1>REACT COMPLETE COURSE</h1>
       {/* <Lists /> */}
-      <AppUser />
+      {/* <AppUser /> */}
       {/* <Forms /> */}
+      <UseEffectHook />
     </div>
   );
 }
