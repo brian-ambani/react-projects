3c1e78aa5d07425aaa8b2ea012192eff7297f4e9
brian-ambani <brainambani1@gmail.com>
2023-11-17 02:56:24 +0300

    updated the counter app

--- react-full-course/contact-app/src/components/Counter.jsx
+++ react-full-course/contact-app/src/components/Counter.jsx
@@ -3,17 +3,12 @@ import { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  const addCount = () => {
-    setCount(count + 1);
-  };
+  const addCount = () => setCount(count + 1);
 
-  const subCount = () => {
-    setCount(count - 1);
-  };
+  const subCount = () => setCount(count - 1);
 
-  const resetCount = () => {
-    setCount(0);
-  };
+  const resetCount = () => setCount(0);
+
   return (
     <div>
       <h1 onClick={addCount}>+</h1>
