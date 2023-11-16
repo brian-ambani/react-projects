bdc5a070337b836526639747829a73911efca709
brian-ambani <brainambani1@gmail.com>
2023-11-16 13:33:55 +0300

    updated counter App

---
+++ react-full-course/contact-app/src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount(count + 1);
+  };
+
+  const subCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+  return (
+    <div>
+      <h1 onClick={addCount}>+</h1>
+      <h1>{count}</h1>
+      <h1 onClick={subCount}>-</h1>
+      <h1 onClick={resetCount}>Reset</h1>
+    </div>
+  );
+}
