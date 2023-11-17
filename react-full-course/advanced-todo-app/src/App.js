8d73465d98463b2f09e2b0cd358c6ca57800b544
brian-ambani <brainambani1@gmail.com>
2023-11-17 10:59:27 +0300

    updated react-icons

--- react-full-course/advanced-todo-app/src/App.js
+++ react-full-course/advanced-todo-app/src/App.js
@@ -13,18 +13,22 @@ function App() {
     // Adding the newTodo to the list
     setTodoList([...todoList, newTask]);
   };
+
+  const deleteTask = (deleteTaskName) => {
+    setTodoList(todoList.filter((todo) => todo.taskName !== deleteTaskName));
+  };
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <TodoInput addTask={addTask} />
-
-      <ul>
-        <li>
+      <h1>Todo App</h1>
+      <TodoInput addTask={addTask} deleteTask={deleteTask} />
+      <div>
+        <ul>
           {todoList.map((todo) => (
             <TaskItem todo={todo} />
           ))}
-        </li>
-      </ul>
+        </ul>
+        {todoList.length === 0 ? <p>You are done!</p> : null}
+      </div>
     </div>
   );
 }
--- react-full-course/advanced-todo-app/src/components/TaskItem.jsx
+++ react-full-course/advanced-todo-app/src/components/TaskItem.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { MdDeleteSweep } from "react-icons/md";
 
-export default function TaskItem({ todo }) {
+const TaskItem = ({ todo, deleteTask }) => {
+  const handleDelete = () => deleteTask(todo.taskName);
+
   return (
     <div>
-      <h3>{todo.taskName}</h3>
+      <li>
+        <div>
+          <p>{todo.taskName}</p>
+        </div>
+
+        <MdDeleteSweep style={{ cursor: "pointer" }} onClick={handleDelete} />
+      </li>
     </div>
   );
-}
+};
+
+export default TaskItem;
