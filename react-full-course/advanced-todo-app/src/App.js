5089dfb2820745baa9d46062c4b9e0ae84cc6db9
brian-ambani <brainambani1@gmail.com>
2023-11-17 09:51:24 +0300

    updated a fumction to create a new todo and add it to the todo list

--- react-full-course/advanced-todo-app/src/App.js
+++ react-full-course/advanced-todo-app/src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import TodoInput from "./components/TodoInput";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const addTask = (taskName) => {
+    // creating a newTodo
+    const newTask = { taskName };
+
+    // Adding the newTodo to the list
+    setTodoList([...todoList, newTask]);
+  };
+  console.log(todoList);
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <TodoInput />
+      <TodoInput addTask={addTask} />
     </div>
   );
 }
