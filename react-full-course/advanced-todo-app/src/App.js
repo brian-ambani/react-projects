a0c13fbac822d63800f756c14eed816b2db60195
brian-ambani <brainambani1@gmail.com>
2023-11-17 10:11:58 +0300

    updated App functiom

--- react-full-course/advanced-todo-app/src/App.js
+++ react-full-course/advanced-todo-app/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import TodoInput from "./components/TodoInput";
+import TaskItem from "./components/TaskItem";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -17,6 +18,14 @@ function App() {
     <div className="App">
       <h1>Hello React</h1>
       <TodoInput addTask={addTask} />
+
+      <ul>
+        <li>
+          {todoList.map((todo) => (
+            <TaskItem todo={todo} />
+          ))}
+        </li>
+      </ul>
     </div>
   );
 }
