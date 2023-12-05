5d7189a1881bfbd7ccb9b02fde38155b1478242c
brian-ambani <brainambani1@gmail.com>
2023-12-05 12:15:07 +0300

    updated the hookhandler

--- react-full-course/contact-app/src/hooks/HookHandler.js
+++ react-full-course/contact-app/src/hooks/HookHandler.js
@@ -1,11 +1,12 @@
 import React from "react";
+import HookArrays from "./HookArrays";
 // import HookCounter from "./HookCounter";
-import HookObject from "./HookObject";
+// import HookObject from "./HookObject";
 
 function HookHandler() {
   return (
     <div>
-      <HookObject />
+      <HookArrays />
     </div>
   );
 }
