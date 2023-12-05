ef72afc8b2b79b64ab32aab0c1f5bb3e33efb672
brian-ambani <brainambani1@gmail.com>
2023-12-05 11:45:02 +0300

    updated the app compement

--- react-full-course/contact-app/src/App.js
+++ react-full-course/contact-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import Forms from "./components/Forms";
-import Lists from "./components/Lists";
-import AppUser from "./components/props/AppUser";
-import UseEffectHook from "./components/UseEffectHook";
-import ComponentA from "./components/contextAPI/ComponentA";
-import AppContext from "./components/useContext/AppContext";
-import Counter from "./components/Counter";
-import Todo from "./todoApp/Todo";
+// import Forms from "./components/Forms";
+// import Lists from "./components/Lists";
+// import AppUser from "./components/props/AppUser";
+// import UseEffectHook from "./components/UseEffectHook";
+// import ComponentA from "./components/contextAPI/ComponentA";
+// import AppContext from "./components/useContext/AppContext";
+// import Counter from "./components/Counter";
+// import Todo from "./todoApp/Todo";
+import HookHandler from "./hooks/HookHandler";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       {/* <AppContext /> */}
       {/* <Counter /> */}
       {/* <Todo /> */}
+      <HookHandler />
     </div>
   );
 }
