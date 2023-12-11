b70ac2166a6ef9045850e0f703f89b95503b2c57
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:10:13 +0300

    updated the services compoonent and route

---
+++ react-website/src/components/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div>
+      <h3>Services</h3>
+      <p>Our services include:</p>
+      <ul>
+        <li>Web Development</li>
+        <li>Mobile Development</li>
+        <li>Cloud Development</li>
+      </ul>
+    </div>
+  );
+}
