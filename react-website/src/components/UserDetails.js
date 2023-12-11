39757c0437dff38f178dde3067172c8563276c86
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:49:31 +0300

    updated user component with ids

---
+++ react-website/src/components/UserDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserDetails() {
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>Check out our user details</p>
+    </div>
+  );
+}
