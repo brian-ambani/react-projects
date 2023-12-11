fc75f3c3ee7ed56396452625614f76474740bbb9
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:48:41 +0300

    updated the nav

--- react-website/src/components/Nav.js
+++ react-website/src/components/Nav.js
@@ -2,10 +2,12 @@ import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="nav-links">
       <NavLink to="/">Home</NavLink>
       <NavLink to="about">About</NavLink>
       <NavLink to="services">Services</NavLink>
+      <NavLink to="products">Products</NavLink>
+      <NavLink to="users">Users</NavLink>
     </nav>
   );
 }
