d095ac076931a0ee7b69e712d55a1090c05dfafb
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:48:21 +0300

    updated a nested route

---
+++ react-website/src/components/NewProducts.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NewProducts() {
+  return (
+    <div>
+      <h1>New Products</h1>
+      <p>Check out our new products</p>
+    </div>
+  );
+}
