57f1893e383212c640e2bcced980d2ae64c777de
brian-ambani <brainambani1@gmail.com>
2023-12-12 00:55:57 +0300

    updated the Users component

--- react-website/src/components/Users.js
+++ react-website/src/components/Users.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export default function Users() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter") === "active";
   return (
     <div>
       <h1>Users</h1>
@@ -10,6 +12,17 @@ export default function Users() {
       <h2>User 2</h2>
       <h2>User 3</h2>
       <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      </div>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
     </div>
   );
 }
