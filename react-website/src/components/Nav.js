881d64b6e64fc79bab2d9b058c001cc9dcd7720f
brian-ambani <brainambani1@gmail.com>
2023-12-12 00:55:12 +0300

    updated the Nav component

--- react-website/src/components/Nav.js
+++ react-website/src/components/Nav.js
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 export default function Nav() {
+  const auth = useAuth();
   return (
     <nav className="nav-links">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="about">About</NavLink>
-      <NavLink to="services">Services</NavLink>
-      <NavLink to="products">Products</NavLink>
-      <NavLink to="users">Users</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/products">Products</NavLink>
+      <NavLink to="/users">Users</NavLink>
+      <NavLink to="/profile">Profile</NavLink>
+      {!auth.user && <NavLink to="/login">Login</NavLink>}
     </nav>
   );
 }
