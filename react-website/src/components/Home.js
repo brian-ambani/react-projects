5fc34a453071d00aeefd6b3ed18d2cc01142bc86
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:12:19 +0300

    updated the home component

--- react-website/src/components/Home.js
+++ react-website/src/components/Home.js
@@ -1,5 +1,12 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h3>Home</h3>
+      <p>Welcome to our site!</p>
+      <button onClick={() => navigate("order-summary")}>Order Now</button>
+    </div>
+  );
 }
