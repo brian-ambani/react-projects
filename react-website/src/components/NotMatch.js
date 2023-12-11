bd5df968e4b7a3b5a19ba37f06b51e04f450fa95
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:11:47 +0300

    updated the notfound route

---
+++ react-website/src/components/NotMatch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotMatch() {
+  return (
+    <div>
+      <h3>Not Found</h3>
+      <p>Sorry, the page you're looking for cannot be found.</p>
+    </div>
+  );
+}
