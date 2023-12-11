a2f5df4ebc747f5f085d390b915839d5e68b4763
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:47:54 +0300

    updated a nested route

---
+++ react-website/src/components/FeaturedProducts.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FeaturedProducts() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <p>Check out our featured products</p>
+    </div>
+  );
+}
