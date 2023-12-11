934b6165f4757d14a8b00e6488891f3160946e73
brian-ambani <brainambani1@gmail.com>
2023-12-11 21:09:12 +0300

    updated the App file

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Nav from "./components/Nav";
+import Services from "./components/Services";
+import OrderSummary from "./components/OrderSummary";
+import NotMatch from "./components/NotMatch";
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NotMatch />} />
       </Routes>
     </div>
   );
