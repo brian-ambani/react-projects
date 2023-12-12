01c246e54d1440bbab133ec39954e88db89136a1
brian-ambani <brainambani1@gmail.com>
2023-12-12 11:31:25 +0300

    updated the nav

---
+++ nav/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Navbar } from "./components/nav-1/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
