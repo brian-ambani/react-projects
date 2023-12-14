46c0ca83db8db88321ad17dbc4f1eef381059c71
brian-ambani <brainambani1@gmail.com>
2023-12-14 14:25:07 +0300

    upsated the App component

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,13 @@
-import { Hook1 } from "./components/hooks/Hook1";
+// import { Hook1 } from "./components/hooks/Hook1";
+import { Hook2 } from "./components/hooks/Hook2";
 
 function App() {
   return (
     <div className="App">
       <>
         <h1>React App</h1>
-        <Hook1 />
+        {/* <Hook1 /> */}
+        <Hook2 />
       </>
     </div>
   );
