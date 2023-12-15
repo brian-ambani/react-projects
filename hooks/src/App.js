4c73bac27b479a8a6d1ae3863c3d785308f8698d
brian-ambani <brainambani1@gmail.com>
2023-12-15 03:23:46 +0300

    updated the useEffect hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,5 +1,8 @@
 // import { Hook1 } from "./components/hooks/Hook1";
-import { Hook2 } from "./components/hooks/Hook2";
+// import { Hook2 } from "./components/hooks/Hook2";
+import { Hook3 } from "./components/hooks/Hook3";
+
+// useRef => allows us to access DOM elements and create mutable variables
 
 function App() {
   return (
@@ -7,7 +10,8 @@ function App() {
       <>
         <h1>React App</h1>
         {/* <Hook1 /> */}
-        <Hook2 />
+        {/* <Hook2 /> */}
+        <Hook3 />
       </>
     </div>
   );
