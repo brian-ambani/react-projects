177a8c3c57afded53d8c6a450383dc261f807f73
brian-ambani <brainambani1@gmail.com>
2023-12-15 03:41:13 +0300

    updated the hook3 component in the App file

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,8 +2,6 @@
 // import { Hook2 } from "./components/hooks/Hook2";
 import { Hook3 } from "./components/hooks/Hook3";
 
-// useRef => allows us to access DOM elements and create mutable variables
-
 function App() {
   return (
     <div className="App">
