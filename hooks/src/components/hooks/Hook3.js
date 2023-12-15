021c8f3db2d417648c3b0c4d47028ada84812d59
brian-ambani <brainambani1@gmail.com>
2023-12-15 03:40:19 +0300

    updated the useRef hook

--- hooks/src/components/hooks/Hook3.js
+++ hooks/src/components/hooks/Hook3.js
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+// useRef => allows us to access DOM elements and create mutable variables
+// useRef => allows us to access DOM elements and create mutable variables
 
 export const Hook3 = () => {
   const [name, setName] = useState("");
+  const count = useRef(0);
+  const inputElement = useRef(null);
+
+  const handleClick = () => {
+    console.log(inputElement);
+    inputElement.current.style.width = "300px";
+    inputElement.current.focus();
+  };
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
   return (
     <div>
       <input type="text" onChange={(e) => setName(e.target.value)} />
       <h2>Name : {name}</h2>
+      <h2>Count : {count.current}</h2>
+      <br />
+
+      <input ref={inputElement} />
+      <button onClick={handleClick}>Click Here</button>
     </div>
   );
 };
