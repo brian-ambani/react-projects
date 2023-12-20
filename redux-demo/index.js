9829a1c69e2e03b623eb17bd76ebcbe90364482d
brian-ambani <brainambani1@gmail.com>
2023-12-21 02:56:02 +0300

    updated the action, state and reducer function

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,27 @@
-console.log("From index.js");
+const BUY_CAKE = "BUY_CAKE";
+
+// Action creator
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+// Reducer: (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, // copy of state object
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
